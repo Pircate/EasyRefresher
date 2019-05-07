1333a0896b61b7984210b2062c59cdb045774e2b
Meow <gao497868860@gmail.com>
2019-05-07 14:26:33 +0800

    Initial commit

---
+++ Example/EasyRefresher/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EasyRefresher
+//
+//  Created by Pircate on 05/07/2019.
+//  Copyright (c) 2019 Pircate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
